061952d385b56128d76ad3452599b303d458024a
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-28 07:45:57 -0700

    Added changes for task 2

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const server = express();
+const port = (process.env.PORT) ? process.env.PORT : 5000;
+
+server.use(express.json());
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
