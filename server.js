1d48b5c7a73ac6d347fb02c0eb2dc5176815f272
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-28 08:09:52 -0700

    Fix 1 for task 2

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = require('./routes/index');
 
 const server = express();
-const port = (process.env.PORT) ? process.env.PORT : 5000;
+const port = process.env.PORT ? process.env.PORT : 5000;
 
 server.use(express.json());
 server.use(router);
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
