c98dbf3b4f86e295536a284b7420c89a4bfe6672
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-28 03:07:34 -0700

    Init files for task 1

---
+++ utils/db.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb');
+const mongo = require('mongodb');
+
+
+class DBClient {
+  constructor () {
+    // Code goes here
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
