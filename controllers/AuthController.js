2490e1b5000022c4819c86c95fe52c66cc037083
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-28 17:56:50 -0700

    Testing refactored coded for task 4

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,36 +1,44 @@
-import { v4 as uuidv4 } from 'uuid';
-
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
-const { hashPassword } = require('../utils/utils');
+import {
+  createHash,
+} from 'crypto';
+import {
+  v4 as uuidv4,
+} from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AuthController {
   static async getConnect(req, res) {
-    // Check if header is available
     const authToken = req.header('Authorization') || null;
     if (!authToken) {
       res.status(401).send({ error: 'Unauthorized' });
       return;
     }
 
-    // Decode auth token from base64 to utf8 to get string containing email and pwd
-    const authTokenDecoded = Buffer.from(authToken.split(' ')[1], 'base64').toString('utf-8');
+    const authTokenDecoded = Buffer.from(authToken.split(' ')[1],
+      'base64').toString('utf8');
     const [email, password] = authTokenDecoded.split(':');
     if (!email || !password) {
       res.status(401).send({ error: 'Unauthorized' });
       return;
     }
-
-    // Check if user exists
-    const hashPwd = hashPassword(password);
-    const user = await (await dbClient.usersCollection()).findOne({ email, password: hashPwd });
+    const hash = createHash('sha1').update(password).digest('hex');
+    const collection = dbClient.db.collection('users');
+    const user = await collection.findOne({
+      email,
+      password: hash,
+    });
     if (user) {
-      // Create new token
       const token = uuidv4();
-      await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-      res.status(200).send({ token });
+      const key = `auth_${token}`;
+      await redisClient.set(key, user._id.toString(), 86400); // 1 day
+      res.status(200).send({
+        token,
+      });
     } else {
-      res.status(401).send({ error: 'Unauthorized' });
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
     }
   }
 
@@ -41,14 +49,16 @@ class AuthController {
       return;
     }
     authToken = `auth_${authToken}`;
-    const user = redisClient.get(authToken);
+    const user = await redisClient.get(authToken);
     if (user) {
       await redisClient.del(authToken);
       res.status(204).send();
     } else {
-      res.status(401).send({ error: 'Unauthorized' });
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
     }
   }
 }
 
-module.exports = AuthController;
+export default AuthController;
