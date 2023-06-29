393f37e3a3a4426f000d1342166bc912e4ef35ce
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-28 18:18:24 -0700

    Added import to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
