e632e5756e1b6bf7d85905122a6ca9ec4cd30519
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-30 11:13:50 -0700

    Added test folder

---
+++ tests/init.test.js
@@ -0,0 +1,9 @@
+/* Initialize all tests */
+import supertest from 'supertest';
+import chai from 'chai';
+import api from '../server';
+
+global.app = api;
+global.request = supertest(api);
+global.expect = chai.expect;
+global.assert = chai.assert;
