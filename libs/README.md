d1c8d896f33cb7dff2a735952937e922747348ce
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-30 10:49:30 -0700

    Added files for all tasks

---
+++ libs/README.md
@@ -0,0 +1,1 @@
+This folder is for supporting functions in ../server.js
---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+
+export default redisClient;
